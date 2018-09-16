731bf574a8945c5a8d6a06bad26a29f051eeb081
snowember <timwestberg43@gmail.com>
2018-09-16 11:37:54 -0700

    updating with most recent changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,30 +11,46 @@ let characters = {
  "obiWan": {
     hitPoints: 400,
     attackPower: 8,
+    powerCl: "#kenobiPower",
     counterPower: 25,
+    imageURl: "assets/images/Obi-Wan",
+    name: "Obi-Wan Kenobi",
+    nameCl: "#vader-n"
 },
 
  "darthMaul": {
     hitPoints: 600,
     attackPower: 7,
+    powerCl: "#maulPower",
     counterPower: 20,
+    imageURl: "assets/images/Darth Maul.jpg",
+    name: "Darth Maul",
+    nameCl: "#vader-n"
 },
 
 "lukeSkywalker": {
     hitPoints: 700,
     attackPower: 5,
+    powerCl: "#lukePower",
     counterPower: 15,
+    imageURl: "assets/images/Luke-skywalker.png",
+    name: "Luke Skywalker",
+    nameCl: "#vader-n"
 },
 
 "darthVader": {
     hitPoints: 800,
     attackPower: 9,
-    counterPower: 30,      
+    powerCl: "#vaderPower",
+    counterPower: 30,    
+    imageURl: "assets/images/Darth-Vader",
+    name: "Darth Vader",
+    nameCl: "#vader-n"
 }
 
 };
 
-console.log(characters)
+console.log(characters.name)
 // let atk = $("#atkButton").on("click", obiWan.atkPower());
 // console.log(atk);
 
@@ -53,10 +69,10 @@ const character = [obi, maul, luke, vader];
 
 // Relfecting the hitpoints to the game
 
-$("#kenobiPower").text(obiWan.hitPoints);
-$("#maulPower").text(darthMaul.hitPoints);
-$("#lukePower").text(lukeSkywalker.hitPoints);
-$("#vaderPower").text(darthVader.hitPoints);
+$("#kenobiPower").text(characters.obiWan.hitPoints);
+$("#maulPower").text(characters.darthMaul.hitPoints);
+$("#lukePower").text(characters.lukeSkywalker.hitPoints);
+$("#vaderPower").text(characters.darthVader.hitPoints);
 
 // function for determining attack power
 
@@ -70,10 +86,15 @@ function atkPower(obj) {
 
 
 let renderOne = function(character, renderArea ) {
-const obi = document.getElementById("obi-wan");
-const maul = document.getElementById("darth-maul");
-const luke = document.getElementById("luke-skywalker");
-const vader = document.getElementById("darth-vader");
+
+let charDiv = $("<div class='character-card' data-name='"+character.name+"'>");
+let charName = $("<div id='"+character.nameCl+"'>").text(character.name);
+let charImage = $("<img class='character-img'  class='img-fluid' alt='Responsive image'>").attr("src", character.imageURl);
+let charHealth = $("<div id='"+character.powerCl+"'>").text(character.health);
+charDiv.append(charName).append(charHealth).append(charImage);
+$(renderArea),append(charDiv);
+
+
   
 
 };
@@ -83,12 +104,14 @@ let renderChar = function(charObj, areaRender) {
         $(areaRender).empty()
     
     for(var key in charObj) {
-        if(character.Obj.hasownproperty(key)) {
+        if(charObj.hasOwnProperty(key)) {
             renderOne(charObj[key], areaRender);
              }
         }
     }
 };
 // end of renderChar
+
+renderChar(characters, "#character-sel");
 
 });
